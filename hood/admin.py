a9b0968aafea617c9f2bb0cca57d48d0989093b3
mary atieno <mercymary 948@gmail.com>
2022-06-22 02:09:17 +0300

    adds content

---
+++ hood/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from hood.models import Profile,Neighbourhood,Post,Business
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
+admin.site.register(Business)
